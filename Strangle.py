28c4c28dc1c764f89a10bc5cd374aaa231b6bbb2
Aeron7 Inc <hello@aeron7.com>
2020-05-12 04:06:24 +0530

    1.4

--- Strangle.py
+++ Strangle.py
@@ -20,15 +20,7 @@ Trade_Date = NSEI['Date']
 Open_Price = NSEI['Open']
 
 
-print(Trade_Date)
 Strangle = leg_tracker(Time_column,Trade_Date,Price_column,Open_Price)
-
-#This function replaces / in dates with - so that it can be matched
-def Replacer(x):
-    a = 0
-    while a < len(x):
-        x[a] = x[a].replace('/','-')
-        a += 1
 
 #This function calculates the Entry Point
 def Entry(x,z,b,m):
@@ -71,16 +63,21 @@ profit_lower = []
 year_list = ['JAN2020','FEB2020','JAN2019','FEB2019','MAR2019','APR2019','MAY2019','JUN2019','JUL2019','AUG2019','SEP2019','OCT2019','NOV2019','DEC2019','']
 k = 0
 while k < (len(Trade_Date)):
-    upperStrike = 'Data/2020/JAN2020/NIFTY' + str(Strangle['Upper Leg'][str(Trade_Date[k])]) + str('CE.csv')
-    lowerStrike = 'Data/2020/JAN2020/NIFTY' + str(Strangle['Lower Leg'][str(Trade_Date[k])]) + str('PE.csv')
-    date_ce = csv_reader(upperStrike,1)
-    date_pe = csv_reader(lowerStrike,1)
-    time_ce = csv_reader(upperStrike,2)
-    time_pe = csv_reader(lowerStrike,2)
-    premium_ce = csv_reader(upperStrike,3)
-    premium_pe = csv_reader(lowerStrike,3)
-    Replacer(date_ce)
-    Replacer(date_pe)
+
+    upperStrike = pd.read_csv('Data/2020/JAN2020/NIFTY' + str(Strangle['Upper Leg'][str(Trade_Date[k])]) + str('CE.csv'))
+    lowerStrike = pd.read_csv('Data/2020/JAN2020/NIFTY' + str(Strangle['Lower Leg'][str(Trade_Date[k])]) + str('PE.csv'))
+
+    date_ce = upperStrike['date']
+    time_ce = upperStrike['time']
+    premium_ce = upperStrike['D']
+
+    date_pe = lowerStrike['date']
+    time_pe = lowerStrike['time']
+    premium_pe = lowerStrike['D']
+
+    date_pe=date_pe.str.replace('/','-')
+    date_ce=date_ce.str.replace('/','-')
+
     entry_upper_strike = Entry(date_ce,time_ce,k,premium_ce)
     entry_lower_strike = Entry(date_pe,time_pe,k,premium_pe)
     if entry_lower_strike is not None or entry_upper_strike is not None:
